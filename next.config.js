05a432fa212746f800c7f331ae81faad262e0535
Fazna <faznaharees@cet.ac.in>
2020-10-20 21:32:20 +0530

    antd trouble

--- next.config.js
+++ next.config.js
@@ -1,38 +1,40 @@
-const fs = require('fs');
-const path = require('path');
 
-const lessToJS = require('less-vars-to-js');
-const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
+const withLess = require('@zeit/next-less')
+const lessToJS = require('less-vars-to-js')
+const fs = require('fs')
+const path = require('path')
 
-const withAntd = require('./next-antd.config');
+// Where your antd-custom.less file lives
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
+)
 
-const antdVariables = lessToJS(fs.readFileSync(path.resolve(__dirname,'./styles/globals.css'), 'utf8'));
+module.exports = withLess({
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+    modifyVars: themeVariables, // make your antd custom effective
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/
+      const origExternals = [...config.externals]
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback()
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback)
+          } else {
+            callback()
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ]
 
-// fix: prevents error when .less files are required by node
-if (typeof require !== 'undefined') {
-  require.extensions['.less'] = file => {};
-}
-
-module.exports =
-  
-    withAntd({
-      cssModules: true,
-      cssLoaderOptions: {
-        sourceMap: false,
-        importLoaders: 1,
-      },
-      lessLoaderOptions: {
-        javascriptEnabled: true,
-        modifyVars: antdVariables,
-      },
-      webpack: config => {
-        config.plugins.push(
-          new FilterWarningsPlugin({
-            // ignore ANTD chunk styles [mini-css-extract-plugin] warning
-            exclude: /mini-css-extract-plugin[^]*Conflicting order between:/,
-          }),
-        );
-
-        return config;
-      },
-    });
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
+      })
+    }
+    return config
+  },
+})
