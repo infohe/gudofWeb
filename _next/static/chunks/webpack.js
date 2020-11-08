a55d61bf5223a7ced0ac5e7dfc952c8df90674c2
Fazna <faznaharees@cet.ac.in>
2020-11-08 10:09:01 +0530

    Deploy gh-pages

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b0d2795f1b57bf20ab51";
+/******/ 	var hotCurrentHash = "4de67ddf79b94830bbbe";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
